988c2c2a8c25c6f80496e4a5931ce3b09f3e29b4
Aleksandr Semenov <avsemenov88@mail.ru>
2025-03-12 15:06:55 +0300

    app.component update

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { RegistrationComponent } from './registration/registration.component';
+import { AuthorizationComponent } from './authorization/authorization.component';
 
 @Component({
   selector: 'app-auth',
-  imports: [],
+  imports: [RegistrationComponent, AuthorizationComponent],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })
 export class AuthComponent {
 
+  
 }
