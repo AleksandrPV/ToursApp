be19e03be1f9d17662dab40b13ff41f3a4f4872b
Aleksandr Semenov <avsemenov88@mail.ru>
2025-03-12 18:39:58 +0300

    auth.components TabsModule is added

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RegistrationComponent } from './registration/registration.component';
 import { AuthorizationComponent } from './authorization/authorization.component';
+import { TabsModule } from 'primeng/tabs';
 
 @Component({
   selector: 'app-auth',
-  imports: [RegistrationComponent, AuthorizationComponent],
+  imports: [RegistrationComponent, AuthorizationComponent, TabsModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })
