f6170f99c26975c328856b0a0ba3da47cee1a8b3
Aleksandr Semenov <avsemenov88@mail.ru>
2025-03-12 18:45:05 +0300

    authorization form is added

--- src/app/pages/auth/authorization/authorization.component.ts
+++ src/app/pages/auth/authorization/authorization.component.ts
@@ -1,12 +1,23 @@
-import { Component} from '@angular/core';
-
+import { NgClass } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 @Component({
   selector: 'app-authorization',
-  imports: [],
+  imports: [
+        NgClass, 
+        FormsModule,
+        ButtonModule,
+        InputTextModule,
+  ],
   templateUrl: './authorization.component.html',
   styleUrl: './authorization.component.scss'
 })
-export class AuthorizationComponent {
+export class AuthorizationComponent implements OnDestroy, OnInit {
+
+  login: string;
+  password: string;
 
   ngOnInit(): void {
     console.log("OnInit -> AuthorizationComponent")
