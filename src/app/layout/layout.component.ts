8b9671c28fc9ffea775002458417af69a36c5d0e
Aleksandr Semenov <avsemenov88@mail.ru>
2025-03-13 20:22:23 +0300

    layout component is added

---
+++ src/app/layout/layout.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "./header/header.component";
+import { AsideComponent } from "./aside/aside.component";
+import { FooterComponent } from "./footer/footer.component";
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  imports: [RouterModule, HeaderComponent, AsideComponent, FooterComponent],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss'
+})
+export class LayoutComponent {
+
+}
