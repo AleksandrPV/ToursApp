ec52030150d97a6c7a372fda9a110a97c2d2e43b
Aleksandr Semenov <avsemenov88@mail.ru>
2025-03-22 10:02:07 +0300

    IApi interface is added

---
+++ src/app/models/IApi.ts
@@ -0,0 +1,7 @@
+export interface IApi {
+    auth: string,
+    registration: string,
+    tours: string,
+    tour: string,
+    config: string,
+}
--- src/app/shared/api/index.ts
+++ src/app/shared/api/index.ts
@@ -1,7 +1,8 @@
 import { environment } from "../../../environments/environment.development";
+import { IApi } from "../../models/IApi";
 
 const serverApi = environment.apiUrl;
-export const API = {
+export const API: IApi = {
     auth: `${serverApi}/auth`,
     registration: `${serverApi}/register`,
     tours: `${serverApi}/tours`,
