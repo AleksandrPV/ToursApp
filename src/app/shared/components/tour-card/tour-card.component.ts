a7a6f732c5faffe421710bfa84fab00d05839f39
Aleksandr Semenov <avsemenov88@mail.ru>
2025-03-22 12:01:27 +0300

    tour-card is added

---
+++ src/app/shared/components/tour-card/tour-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tour-card',
+  imports: [],
+  templateUrl: './tour-card.component.html',
+  styleUrl: './tour-card.component.scss'
+})
+export class TourCardComponent {
+
+}
