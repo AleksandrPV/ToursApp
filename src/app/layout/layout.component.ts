510d48802290a88fa22ad22603c73d50bb734c0c
Aleksandr Semenov <avsemenov88@mail.ru>
2025-03-23 12:25:32 +0300

    styles is updated

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -6,6 +6,7 @@ import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
+  standalone: true,
   imports: [RouterModule, HeaderComponent, AsideComponent, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
