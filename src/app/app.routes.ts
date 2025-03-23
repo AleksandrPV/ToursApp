2eb3e39b25f576164fde30f4c394ecaf0b6e84f1
Aleksandr Semenov <avsemenov88@mail.ru>
2025-03-23 20:32:39 +0300

    authGuard is added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ToursComponent } from './pages/tours/tours.component';
 import { TourItemComponent } from './pages/tour-item/tour-item.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFound404Component } from './pages/not-found-404/not-found-404.component';
+import { authGuard } from './shared/guards/auth.guard';
 
 
 export const routes: Routes = [
@@ -12,7 +13,10 @@ export const routes: Routes = [
 { path: "", component: LayoutComponent, children: [
     { path: "", component: HomeComponent},
 ]},
-{ path: "tours", component: LayoutComponent, children: [
+{ path: "tours", 
+    canActivate: [authGuard],
+    component: LayoutComponent,
+    children: [
     { path: "", component: ToursComponent},
     { path: "tour/:id", component: TourItemComponent},
     { path: "tour", redirectTo: '', pathMatch: 'full'},
